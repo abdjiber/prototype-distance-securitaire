17224df5cd18920750f79151260d2340f2f3ae33
DJIBEROU MAHAMADOU Abdoul Jalil <abdouljalilm@gmail.com>
2020-11-03 16:00:47 +0100

    Formatting Python code

--- app web/sql/users.sql
+++
@@ -1,7 +0,0 @@
-CREATE TABLE dbprototype.USERS(
-    id VARCHAR(60),
-    city VARCHAR(200),
-    latitude DOUBLE,
-    longitude DOUBLE,
-    CONSTRAINT CP PRIMARY KEY(id)
-    );
--- app web/src/connection_info.py
+++
@@ -1,5 +0,0 @@
-DB_HOST = "db4free.net"
-DB_USER_NAME = "prototype"
-DB_USER_PASS_WORD = "Prototype"
-DB_NAME = "dbprototype"
-TABLE_NAME = "USERS"
