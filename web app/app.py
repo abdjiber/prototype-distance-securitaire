24d827d97f297212d3ce4c7c9f14b5a15273cf18
DJIBEROU MAHAMADOU Abdoul Jalil <abdouljalilm@gmail.com>
2020-11-07 19:30:36 +0100

    Formatting code with YAPF

--- web app/app.py
+++ web app/app.py
@@ -16,8 +16,7 @@ db = DB(host=connection_info.DB_HOST,
         db_name=connection_info.DB_NAME,
         table_name=connection_info.TABLE_NAME,
         user_name=connection_info.DB_USER_NAME,
-        user_pwd=connection_info.DB_USER_PASS_WORD
-        )
+        user_pwd=connection_info.DB_USER_PASS_WORD)
 user = USER()
 
 
@@ -31,8 +30,8 @@ def connection():
     city = flask.request.form['ville']
     min_distance = flask.request.form['min_distance']
     uuid = flask.request.form["uuid"]
-    user.setInfo(uuid, city, min_distance, is_active=True)
-    _insertIntoDB()
+    user.set_info(uuid, city, min_distance, is_active=True)
+    _insert_into_db()
     return flask.jsonify({})
 
 
@@ -44,33 +43,32 @@ def real_time():
 @app.route('/realtime/start', methods=['GET', 'POST'])
 def real_time_start():
     if user.is_active:
-        db.updatePosition(user)
-        res_users_same_city = db.getUsersSameCity(user.city)
+        db.update_position(user)
+        res_users_same_city = db.get_users_same_city(user.city)
         distances = compute_distances(user, res_users_same_city)
         alert, min_dist = set_alert(user.min_distance, distances)
-        response = flask.jsonify(
-                                 {"alert": alert*1,
-                                  "dist_closest_user": round(min_dist, 2)
-                                  }
-                                )
+        response = flask.jsonify({
+            "alert": alert * 1,
+            "dist_closest_user": round(min_dist, 2)
+        })
     else:
         response = flask.jsonify({"dist_closest_user": 10000})
     return response
 
 
 @app.route('/realtime/latlng', methods=['GET', 'POST'])
-def getLatLngFromJS():
+def get_lat_lng_from_js():
     lat = float(flask.request.form['lat'])
     lng = float(flask.request.form['lng'])
-    user.setPosition(Position(lat=lat, lng=lng))
+    user.set_position(Position(lat=lat, lng=lng))
     return flask.jsonify({})
 
 
 @app.route('/realtime/stop', methods=['POST', 'GET'])
 def real_time_stop():
-    user.setPreviousMinDistanceFromOtherUsers(10000.0)
-    user.setIsActive(False)
-    db.deleteFromDB(user.id)
+    user.set_previous_min_distance_from_other_users(10000.0)
+    user.set_is_active(False)
+    db.delete_from_db(user.id)
     return flask.jsonify({})
 
 
@@ -79,12 +77,12 @@ def compute_distances(current_user, res_users_same_city):
     for user in res_users_same_city:
         if user["id"] != current_user.id:
             latlng_user = (float(user["latitude"]), float(user["longitude"]))
-            dist = current_user.position.gmDistance(latlng_user)
+            dist = current_user.position.google_maps_distance(latlng_user)
             distances.append(dist)
     return distances
 
 
-def _insertIntoDB():
+def _insert_into_db():
     failure = True
     try:
         db.insert_user_into_db(user)
@@ -99,7 +97,7 @@ def set_alert(current_user_min_distance, distances):
     min_dist = user.previous_min_dist_from_other_users
     if distances != []:
         min_dist = min(distances)
-        user.setPreviousMinDistanceFromOtherUsers(min_dist)
+        user.set_previous_min_distance_from_other_users(min_dist)
         if min_dist <= current_user_min_distance:
             alert = True
     return alert, min_dist
